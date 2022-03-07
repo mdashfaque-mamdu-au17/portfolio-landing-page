f7eae17c9607bc6e22fbfffe871a9073b79ef609
mdashfaque <ashfaquemamdu666@gmail.com>
2022-03-07 23:58:14 +0530

    Mobile navigation added

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { HashLink } from 'react-router-hash-link';
+import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
 import data from './data';
 import './navbar.scss';
 const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
   return (
     <nav className="navbar__main__container">
       <div className="navbar__logo">
@@ -26,10 +28,51 @@ const Navbar = () => {
             );
           }
         })}
-        <Link to="/works" className="mobile__link">
+        {/* <Link to="/works" className="mobile__link">
           works
-        </Link>
+        </Link> */}
+        <RiMenu3Line
+          color="#fff"
+          size={30}
+          className="mobile__link"
+          onClick={() => setMenuOpen(true)}
+        />
       </div>
+      {menuOpen && (
+        <div className="mobile__menu__container">
+          <RiCloseLine
+            color="#fff"
+            size={30}
+            className="mobile__close__btn"
+            onClick={() => setMenuOpen(false)}
+          />
+          <div className="mobile__nav__links__container slide-bottom">
+            <div className="mobile__nav__links">
+              {data.map((link) => {
+                const { id, name, to } = link;
+                if (name === 'works') {
+                  return (
+                    <Link key={id} to={to} className="nav__links__mobile">
+                      {name}
+                    </Link>
+                  );
+                } else {
+                  return (
+                    <HashLink
+                      key={id}
+                      smooth
+                      to={to}
+                      className="nav__links__mobile"
+                    >
+                      {name}
+                    </HashLink>
+                  );
+                }
+              })}
+            </div>
+          </div>
+        </div>
+      )}
     </nav>
   );
 };
